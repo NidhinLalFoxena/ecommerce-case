06b82a05788fb8bc245902acf4380e2095ce3ab7
NidhinLal <nidhin.lal@foxena.com>
2024-08-13 23:41:50 +0530

    the lading started

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,20 +1,23 @@
 import { cn } from "@/lib/utils";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
-function MaxWidthWrapper({
+const MaxWidthWrapper = ({
   className,
   children,
 }: {
   className?: string;
   children: ReactNode;
-}) {
+}) => {
   return (
     <div
-      className={cn("h-full mx-auto w-full max-w-screen-xl px-2.5  md:px-20")}
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
     >
       {children}
     </div>
   );
-}
+};
 
 export default MaxWidthWrapper;
